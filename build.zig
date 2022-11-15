89696706da665aa11622a09a815ea7f051ab0e9d
Tim de Jager <tim@prefix.dev>
2022-11-15 09:50:27 +0100

    fix: commented build file for clarity

--- build.zig
+++ build.zig
@@ -13,8 +13,8 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
+    // Get the conda prefix from the environment and create the correct environement variables
     const conda_prefix = std.process.getEnvVarOwned(allocator, "CONDA_PREFIX") catch "";
-
     const lib_path = std.fmt.allocPrint(allocator, "{s}/lib", .{conda_prefix}) catch "";
     const include_path = std.fmt.allocPrint(allocator, "{s}/include", .{conda_prefix}) catch "";
 
@@ -23,8 +23,11 @@ pub fn build(b: *std.build.Builder) void {
     exe.setBuildMode(mode);
     exe.linkSystemLibrary("SDL2");
     exe.linkSystemLibrary("c");
+    // This tells zig to look at the conda path for libraries
     exe.addLibraryPath(lib_path);
+    // ... the same for the include path
     exe.addIncludePath(include_path);
+    // and we need to add the libraries to the rpath, otherwise we get a runtime error
     exe.addRPath(lib_path);
     exe.install();
 
